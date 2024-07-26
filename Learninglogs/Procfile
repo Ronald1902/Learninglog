02ebb7de58da81ad1cc6728cadb80c5536c161cf
Ronald1902 <r4540737@gmail.com>
2024-07-26 23:09:39 +0100

    Ready for deployment to heroku.

---
+++ Learninglogs/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Learninglog.wsgi --log-file - 
---
+++ users/urls.py
@@ -0,0 +1,18 @@
+"""Defines URL patterns for users"""
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Login Page.
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # Registration page
+    path('register/', views.register, name='register'),
+
+    # Logout Page.
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
