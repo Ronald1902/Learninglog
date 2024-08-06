ffaa3eadeda8ce63975fd4374eb1d6a196c6784c
Ronald1902 <166233693+Ronald1902@users.noreply.github.com>
2024-08-06 23:00:41 +0100

    Update settings.py

--- Learninglog/settings.py
+++ Learninglog/settings.py
@@ -154,7 +154,7 @@ BOOTSTRAP3 = {
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Heroku settings
